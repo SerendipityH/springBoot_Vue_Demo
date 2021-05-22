35ac243e1ce08b6be67c5ca52c2369efafc557fa
serendipity <2274734747@qq.com>
2021-05-22 15:07:08 +0800

    init

---
+++ vue/demo/src/views/Demo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="demo">
+    <h1>{{result}}</h1>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Demo',
+    data(){
+      return{
+        result:''
+      }
+    },
+    created() {
+      this.$ajax('http://localhost:8080/hello').then(res => {
+        this.result = res.data;
+        console.log(res.data);
+      }).catch(function (error) {
+        console.log(error);
+      });
+    }
+  };
+</script>
